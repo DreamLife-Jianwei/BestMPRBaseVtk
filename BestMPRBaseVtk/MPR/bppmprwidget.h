a899bd096b1f554094cf0c718450174f7706b822
张建伟 <Jianwei1992@foxmail.com>
2021-10-12 10:21:08 +0800

    更新代码

--- BestMPRBaseVtk/MPR/bppmprwidget.h
+++ BestMPRBaseVtk/MPR/bppmprwidget.h
@@ -7,11 +7,35 @@ QT_BEGIN_NAMESPACE
 namespace Ui { class bPPMPRWidget; }
 QT_END_NAMESPACE
 
+enum showType
+{
+    TRA,
+    SAG,
+    COR
+};
+
+
 class bPPMPRWidget : public QWidget
 {
     Q_OBJECT
 
 public:
+
+    int scaleIndex = 0;                             //当前图层
+    int scaleCount = 0;                             //图层数
+    int scaleThickness = 0;                         //层厚
+    int scaleGap = 0;                               //层间距
+
+    QColor imageColor = QColor(255,255,255,255);    //图像颜色
+
+
+    bool scaleTool = false;                         //比例尺工具
+    bool zoomTool = false;                          //放大镜工具
+    bool patientInforTool = false;                  //病人信息
+
+
+   showType showMode= showType::COR;                //显示模式
+
     bPPMPRWidget(QWidget *parent = nullptr);
     ~bPPMPRWidget();
 
